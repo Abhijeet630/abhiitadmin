c38094e38a053a09f9ab2f5fa6f29aec52396a64
Abhijeet630 <abhijeetkhandekar218@gmail.com>
2025-08-06 15:30:57 +0530

    Auto update from VS Code

--- main.py
+++ main.py
@@ -228,4 +228,4 @@ def receive_backup_status():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
